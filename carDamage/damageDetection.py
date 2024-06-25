4deacd17138d4d5e7e8163b8425ee5b6bf826215
thearhansheth <arhan.sheth@gmail.com>
2024-06-25 15:55:49 +0530

    Modified code to save Naive Bayes Model and work on sample input

--- carDamage/damageDetection.py
+++ carDamage/damageDetection.py
@@ -8,6 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import cv2 as cv
+import pickle
 
 #Loading datasets from local directory
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -63,26 +64,31 @@ for image_batch, labels_batch in test_list:
 test_image = test_image.numpy()
 test_label = test_label.numpy()
 
+train_image_resized = [cv.resize(img, (256, 256)) for img in train_image]
+train_image_resized = np.array(train_image_resized)
 #Converting into 2d arrays for scikit-learn
 # Converting train data
-nsamples, nx, ny, nrgb = train_image.shape
-train_image2 = train_image.reshape(nsamples, (nx * ny * nrgb))
+nsamples, nx, ny, nrgb = train_image_resized.shape
+#print(f"Training data shape: {train_image_resized.shape}")
+train_image2 = train_image_resized.reshape(nsamples, (nx * ny * nrgb))
+#print(f"Reshaped training data shape: {train_image2.shape}")
 
 # Converting test data
 nsamples, nx, ny, nrgb = test_image.shape
 test_image2 = test_image.reshape(nsamples, (nx * ny * nrgb))
 
-
 #Converting Validation data into 2d Array
 nsamples, nx, ny, nrgb = validation_image.shape
 validation_image2 = validation_image.reshape(nsamples, (nx * ny * nrgb))
-
+'''
 # Loading Random Forest Model
 rF = RandomForestClassifier()
 rF.fit(train_image2, train_label)
 # Making prediction using Random Forest
 rF_pred = rF.predict(test_image2)
 print("Accuracy Score Random Forest:", accuracy_score(rF_pred, test_label))
+rF.save("/Users/arhan.sheth/Documents/Codes/DX/dx_imgClassification/carDamage/rF_MODEL.h5")
+print("Random Forest Model Saved")
 
 # Loading KNN Model
 knn = KNeighborsClassifier(n_neighbors = 5)
@@ -97,15 +103,17 @@ dt.fit(train_image2, train_label)
 # Making prediction using Decision Tree
 dt_pred = dt.predict(test_image2)
 print("Accuracy Score Decision Tree:", accuracy_score(dt_pred, test_label))
+'''
+# # Loading Naive Bayes Model
+# nb = GaussianNB()
+# nb.fit(train_image2, train_label)
+# # Making prediction using Naive Bayes
+# nb_pred = nb.predict(test_image2)
+# print("Accuracy Score Naive Bayes:", accuracy_score(nb_pred, test_label))
+# with open('nB_model.pkl', 'wb') as f:
+#   pickle.dump(nb, f)
 
-# Loading Naive Bayes Model
-nb = GaussianNB()
-nb.fit(train_image2, train_label)
-# Making prediction using Naive Bayes
-nb_pred = nb.predict(test_image2)
-print("Accuracy Score Naive Bayes:", accuracy_score(nb_pred, test_label))
-
-
+'''
 #Making predictions for validation data
 rF_val_pred = rF.predict(validation_image2)
 print("Accuracy Report Random Forest (Validation):", accuracy_score(rF_val_pred, validation_label))
@@ -118,3 +126,25 @@ print("Accuracy Report Decision Tree (Validation):", accuracy_score(dt_val_pred,
 
 nb_val_pred = nb.predict(validation_image2)
 print("Accuracy Report Naive Bayes (Validation):", accuracy_score(nb_val_pred, validation_label))
+'''
+
+with open("nb_model.pkl", 'rb') as f:
+  loaded_model = pickle.load(f)
+
+#Load custom input image
+img = cv.imread("/Users/arhan.sheth/Documents/Codes/DX/dx_imgClassification/carDamage/sampleInput.jpeg")
+
+#process image
+img_arr = cv.resize(img, (256, 256))
+nx, ny, nrgb = img_arr.shape
+#print(f"Inference image resized shape: {img_arr.shape}")
+#print("nx:", nx, "ny:", ny, "nrgb:", nrgb)
+#print("Total:", (nx * ny * nrgb))
+img_arr2 = img_arr.reshape(1, (nx * ny * nrgb))
+#print(f"Inference image reshaped shape: {img_arr2.shape}")
+
+#declare classes for comparision 
+classes = ["minor", "moderate", "severe"]
+ans = loaded_model.predict(img_arr2)
+#print the class of the predicted
+print("Predicted Class: ", classes[ans[0]]) 
